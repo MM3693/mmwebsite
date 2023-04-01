ce57bd558dfb3e779de3eca4743c3ef5070b8446
Salman Inayat <salmaninayat1999@gmail.com>
2023-04-01 20:23:36 +0500

    Removed global.scss

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Stats from "./pages/Stats/Stats";
 
-import "./assets/scss/global.scss";
-
 function App() {
   return (
     <>
